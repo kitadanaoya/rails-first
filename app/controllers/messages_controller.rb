d603378158f09b53fc565417d0c8a5608435fe51
kitadanaoya <naoya0206k@yahoo.co.jp>
2017-08-05 01:16:04 +0000

    add colun

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -47,7 +47,7 @@ class MessagesController < ApplicationController
   
   private 
   def message_params
-    params.require(:message).permit(:content)
+    params.require(:message).permit(:content, :title)
   end
   
   def set_message
